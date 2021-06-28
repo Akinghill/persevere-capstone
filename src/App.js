16e4ec1d543422f051cfec9b44dea4979fb1d0ed
akinghill <austinkinghill@gmail.com>
2021-06-28 13:53:35 -0700

    Create footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       Capstone 
+      <Footer/>
     </div>
   );
 }
